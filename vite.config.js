366be360c742eb72670b08abde9595712131b2cd
leongpet <leongj.peter@gmail.com>
2025-10-29 15:45:27 -0400

    prepare githubpages for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Personal_Portfolio/',
   server: {
-    host: true, // Needed for Docker
+    host: true,
     port: 3000
   }
 })
